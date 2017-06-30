40b68c8d97fd250a99cb450b38a42c0e31d867eb
wk645 <wk645@nyu.edu>
2017-06-29 20:46:48 -0400

    dog

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+	attr_accessor :name
+
+	@@all = []
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.clear_all
+		@@all.clear
+	end
+	# a.clear
+
+	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+
+
+
+end
